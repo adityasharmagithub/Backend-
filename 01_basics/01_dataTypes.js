0db55ae6db3dd8e3d7ac3c6bd24b333ba16b125b
adityasharmagithub <159531728+adityasharmagithub@users.noreply.github.com>
2024-07-19 18:38:10 +0000

    datatype config done

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all the code of js as new version
+
+console.log(3+3)//code readability
+
+let age =21
+console.log("Aditya")
+
+
+// types of datatypes in JS
+
+// 1. number => 2 to power 53
+// 2. bigint
+// 3. string=>""
+// 4. boolean=> true/false
+// 5. null=>standalone value
+// 6. symbol=>unique
+// 7. object
+
+
+console.log(typeof age);
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail ="aadi272831@gmail"
+var accountPassword ="3451"
+acountCity = "Pathankot"
+let accountState;
+
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
