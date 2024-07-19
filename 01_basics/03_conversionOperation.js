acf32b12e7be20006a3160b0e73452d82ebd05ea
adityasharmagithub <159531728+adityasharmagithub@users.noreply.github.com>
2024-07-19 19:01:00 +0000

    conversion config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+/*let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(valueInNumber);
+
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(valueInNumber);
+
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(valueInNumber);
+
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(valueInNumber);
+
+let score = false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(valueInNumber);
+
+let score = "Aditya" 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);*/
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(stringNumber);
